a4aabcdfc2332acb302d4ca07b0380b8ad36fa47
Zaiste <oh@zaiste.net>
2022-09-07 21:19:21 +0200

    example: add Preact w/ JSX example

---
+++ examples/preact-jsx/App.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'preact';
+
+const App: FunctionComponent = ({ children }) => {
+  return (
+    <div>
+      <nav>Navigation</nav>
+      {children}
+    </div>
+  )
+}
+
+export default App
---
+++ examples/preact-jsx/main.tsx
@@ -0,0 +1,22 @@
+import { serve } from "wren/mod.ts";
+import { GET } from "wren/route.ts";
+import * as Response from "wren/response.ts"
+import render from 'preact-render-to-string'
+
+import Banner from './Banner.tsx';
+import App from './App.tsx';
+
+const routes = [
+  GET('/', () => Response.OK('JSX How-To')), // implicit return
+  GET('/jsx', () => {
+    return Response.HTML(render(
+      <App>
+        <Banner />
+      </App>
+    ));
+  })
+];
+
+
+serve(routes, { port: 3000 });
+
