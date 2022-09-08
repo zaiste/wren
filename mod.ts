63e38c3835ea23ec81d5697a9edb24443f325aef
Zaiste <oh@zaiste.net>
2022-09-08 19:52:38 +0200

    task: harmonize the formatting

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
-import { serve as upstreamServe, ServeInit } from "http/server.ts";
-import { Routing } from "./routing.ts";
-import { Routes } from "./types.ts";
+import { serve as upstreamServe, ServeInit } from 'http/server.ts';
+import { Routing } from './routing.ts';
+import { Routes } from './types.ts';
 
 // FIXME as global middlewares as an array
 export function serve(routes: Routes, options: ServeInit = {}) {
-  const routing = Routing(routes);
-  return upstreamServe(routing, options)
+	const routing = Routing(routes);
+	return upstreamServe(routing, options);
 }
--- route.ts
+++ route.ts
@@ -1,56 +1,72 @@
-import type {
-  Handler,
-  RouteOptions,
-  Route,
-  Middleware,
-  Pipeline,
-  Meta,
-  HTTPMethod,
-} from "./types.ts";
-
-export function isPipeline(handler: Handler | Pipeline): handler is Pipeline {
-  return Array.isArray(handler);
-}
+import {
+	Handler,
+	HTTPMethod,
+	isPipeline,
+	Meta,
+	Middleware,
+	Pipeline,
+	Route,
+	RouteOptions,
+} from './types.ts';
 
 function makeRoute(
-  name: HTTPMethod,
-  path: string,
-  handler: Handler | Pipeline,
-  middleware: Middleware[],
-  meta: Meta
+	name: HTTPMethod,
+	path: string,
+	handler: Handler | Pipeline,
+	middleware: Middleware[],
+	meta: Meta,
 ): Route {
-  if (isPipeline(handler)) {
-    const h = handler.pop() as Handler;
-    return [
-      path,
-      {
-        [name]: h,
-        middleware: [...middleware, ...(handler as Middleware[])],
-        meta,
-      },
-    ];
-  } else {
-    return [path, { [name]: handler, middleware, meta }];
-  }
+	if (isPipeline(handler)) {
+		const h = handler.pop() as Handler;
+		return [
+			path,
+			{
+				[name]: h,
+				middleware: [...middleware, ...(handler as Middleware[])],
+				meta,
+			},
+		];
+	} else {
+		return [path, { [name]: handler, middleware, meta }];
+	}
 }
 
-
-export function GET(path: string, handler: Handler | Pipeline, { middleware = [], meta = {} }: RouteOptions = {}): Route {
-  return makeRoute("GET", path, handler, middleware, meta);
+export function GET(
+	path: string,
+	handler: Handler | Pipeline,
+	{ middleware = [], meta = {} }: RouteOptions = {},
+): Route {
+	return makeRoute('GET', path, handler, middleware, meta);
 }
 
-export function POST(path: string, handler: Handler | Pipeline, { middleware = [], meta = {} }: RouteOptions = {}): Route {
-  return makeRoute("POST", path, handler, middleware, meta);
+export function POST(
+	path: string,
+	handler: Handler | Pipeline,
+	{ middleware = [], meta = {} }: RouteOptions = {},
+): Route {
+	return makeRoute('POST', path, handler, middleware, meta);
 }
 
-export function PATCH(path: string, handler: Handler | Pipeline, { middleware = [], meta = {} }: RouteOptions = {}): Route {
-  return makeRoute("PATCH", path, handler, middleware, meta);
+export function PATCH(
+	path: string,
+	handler: Handler | Pipeline,
+	{ middleware = [], meta = {} }: RouteOptions = {},
+): Route {
+	return makeRoute('PATCH', path, handler, middleware, meta);
 }
 
-export function PUT(path: string, handler: Handler | Pipeline, { middleware = [], meta = {} }: RouteOptions = {}): Route {
-  return makeRoute("PUT", path, handler, middleware, meta);
+export function PUT(
+	path: string,
+	handler: Handler | Pipeline,
+	{ middleware = [], meta = {} }: RouteOptions = {},
+): Route {
+	return makeRoute('PUT', path, handler, middleware, meta);
 }
 
-export function DELETE(path: string, handler: Handler | Pipeline, { middleware = [], meta = {} }: RouteOptions = {}): Route {
-  return makeRoute("DELETE", path, handler, middleware, meta);
+export function DELETE(
+	path: string,
+	handler: Handler | Pipeline,
+	{ middleware = [], meta = {} }: RouteOptions = {},
+): Route {
+	return makeRoute('DELETE', path, handler, middleware, meta);
 }
