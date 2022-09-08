0b04b04704cfd0b5ae0ee451f1c5923facecd454
Zaiste <oh@zaiste.net>
2022-09-08 19:49:39 +0200

    feature: add `ANY` route wildcard

--- router.ts
+++ router.ts
@@ -1,34 +1,41 @@
-import { Handler } from "./types.ts";
+import { Handler } from './types.ts';
 
 interface HandlerMatch {
-  pattern: URLPattern
-  handler: Handler
+	pattern: URLPattern;
+	handler: Handler;
 }
 
-type MatchResponse = [Handler, Record<string, string>] | []
+type MatchResponse = false | {
+	handler: Handler;
+	params: Record<string, string>;
+};
 
 export class Router {
-  #routes: Record<string, HandlerMatch[]> = {
-    GET: [],
-    HEAD: [],
-    PATCH: [],
-    POST: [],
-    PUT: [],
-    DELETE: []
-  }
+	#routes: Record<string, HandlerMatch[]> = {
+		ANY: [],
+		GET: [],
+		HEAD: [],
+		PATCH: [],
+		POST: [],
+		PUT: [],
+		DELETE: [],
+	};
 
-  add(method: string, pathname: string, handler: Handler) {
-    this.#routes[method].push({ pattern: new URLPattern({ pathname }), handler });
-  }
+	add(method: string, pathname: string, handler: Handler) {
+		this.#routes[method].push({
+			pattern: new URLPattern({ pathname }),
+			handler,
+		});
+	}
 
-  find(method: string, url: string): MatchResponse {
-    for (const r of this.#routes[method]) {
-      if (r.pattern.test(url)) {
-        const pathParams = r.pattern.exec(url)!.pathname.groups;
-        return [r.handler, pathParams]
-      }
-    }
+	find(method: string, url: string): MatchResponse {
+		for (const r of this.#routes[method]) {
+			if (r.pattern.test(url)) {
+				const pathParams = r.pattern.exec(url)!.pathname.groups;
+				return { handler: r.handler, params: pathParams };
+			}
+		}
 
-    return []
-  }
+		return false;
+	}
 }
